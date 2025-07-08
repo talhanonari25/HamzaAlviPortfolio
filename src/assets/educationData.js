f4c45cdda836ffafe38b1468f6ace776502ba351
saeedhaider238 <saeed.haider@burntbrains.com>
2025-07-08 20:20:21 +0500

    my resume ui changes

---
+++ src/assets/educationData.js
@@ -0,0 +1,22 @@
+export const eduData = [
+    {
+        timeSpan: "2004 - 2009",
+        designation: "Abc University of Los Angeles",
+        mainData: "I would gladly pay over 600 dollars for tavonline. Tavonline has really helped our business. I like tavonline more and more each day because it makes my life a lot easier."
+    },
+    {
+        timeSpan: "2004 - 2009",
+        designation: "Abc University of Los Angeles",
+        mainData: "I would gladly pay over 600 dollars for tavonline. Tavonline has really helped our business. I like tavonline more and more each day because it makes my life a lot easier."
+    },
+    {
+        timeSpan: "2004 - 2009",
+        designation: "Abc University of Los Angeles",
+        mainData: "I would gladly pay over 600 dollars for tavonline. Tavonline has really helped our business. I like tavonline more and more each day because it makes my life a lot easier."
+    },
+    {
+        timeSpan: "2004 - 2009",
+        designation: "Abc University of Los Angeles",
+        mainData: "I would gladly pay over 600 dollars for tavonline. Tavonline has really helped our business. I like tavonline more and more each day because it makes my life a lot easier."
+    },
+]
