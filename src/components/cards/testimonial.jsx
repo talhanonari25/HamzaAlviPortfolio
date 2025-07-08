bf52a2bd02ece69776048599db8f15d1ea2edd7c
saeedhaider238 <saeed.haider@burntbrains.com>
2025-07-08 20:15:41 +0500

    people saying ui changes

---
+++ src/components/cards/testimonial.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card } from 'antd';
+
+const card = ({comment, author}) => {
+  return (
+
+      <>
+          <Card variant="borderless" style={{ width: 300, backgroundColor: 'rgba(0,0,0,0)' }}>
+              <div style={{display: "flex"}}>
+                <img src="icons/quotes.svg" alt="" style={{alignSelf: "start"}} />
+                <div className="authorInfo">
+                   <p style={{color: "white"}}>{comment}</p>
+                   <hr className="annotationRuler" />
+                   <h5 style={{color: "white"}}>{author}</h5>
+               </div>
+              </div>
+          </Card>
+      </>
+  )
+}
+
+export default card
