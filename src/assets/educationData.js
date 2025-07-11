683e6649e749c4d1291f96d2b93102007fde8eeb
saeedhaider238 <saeed.haider@burntbrains.com>
2025-07-11 20:52:49 +0500

    changed data files

--- src/assets/educationData.js
+++ src/assets/educationData.js
@@ -2,11 +2,13 @@ export const eduData = [
     {
         timeSpan: "2015 - 2017",
         designation: "Punjab Group of Colleges",
-        mainData: "I completed my Intermediate in Pre-Engineering from Punjab Group of Colleges between 2015 and 2017. This laid a strong foundation in mathematics, physics, and chemistry, preparing me for further studies in engineering and technology."
+        mainData: "I completed my Intermediate in Pre-Engineering from Punjab Group of Colleges between 2015 and 2017. This laid a strong foundation in mathematics, physics, and chemistry, preparing me for further studies in engineering and technology.",
+        position: "left"
     },
     {
         timeSpan: "2017 - 2021",
         designation: "Lahore Garrison University",
-        mainData: "I earned my Bachelor's degree in Computer Science from Lahore Garrison University between 2017 and 2021. During this time, I gained in-depth knowledge of programming, data structures, algorithms, and software development practices."
+        mainData: "I earned my Bachelor's degree in Computer Science from Lahore Garrison University between 2017 and 2021. During this time, I gained in-depth knowledge of programming, data structures, algorithms, and software development practices.", 
+        position: "right"
     }
 ]
--- src/assets/expData.js
+++ src/assets/expData.js
@@ -2,20 +2,24 @@ export const data = [
     {
         timeSpan: "2019 - 2020",
         designation: "Codehoppers",
+        position:"left",
         mainData: "During my recent journey, I embarked on a comprehensive internship that encompassed both ends of the technological spectrum. I delved into the MERN (MongoDB, Express.js, React.js, Node.js) stack, crafting dynamic and responsive web applications. Employing HTML, I harnessed the power of React.js to conceptualize and implement seamless user interfaces, breathing life into my creations."
     },
     {
         timeSpan: "2020 - 2023",
+        position:"right",
         designation: "Xprolabs",
         mainData: "I have gained valuable and diverse experience during my tenure at the Xprolabs, contributing to both Rails and MERN Stack projects. My involvement in Rails development has allowed me to architect robust and scalable back-end solutions, leveraging the framework's powerful capabilities to create efficient web applications."
     },
     {
         timeSpan: "2023 - 2024",
+        position:"left",
         designation: "Tkxel",
         mainData: "I am actively engaged in a pivotal role, contributing to a Rails project that follows a sophisticated microservice architecture. This architecture is designed to encapsulate distinct functionalities into separate, independently deployable services, fostering modularity and scalability."
     },
     {
         timeSpan: "2024 - Present",
+        position:"right",
         designation: "RevelAi Health",
         mainData: "I design scalable architectures and build robust backend systems to support core operations. I ensure seamless integration across services, implement multi-tenancy solutions, and lead cross-team technical projects. I also optimize performance and introduce technologies to enhance development."
     }
